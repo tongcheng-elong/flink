eda920e2b2c37602670efe4f7cc79d8503f64192
wxp4532 <wxp4532@ly.com>
2021-08-23 14:57:54 +0800

    fix run application exception handle

--- flink-clients/src/main/java/org/apache/flink/client/deployment/application/ApplicationDispatcherBootstrap.java
+++ flink-clients/src/main/java/org/apache/flink/client/deployment/application/ApplicationDispatcherBootstrap.java
@@ -254,7 +254,7 @@ public class ApplicationDispatcherBootstrap implements DispatcherBootstrap {
                 jobIdsFuture.complete(applicationJobIds);
             }
         } catch (Throwable t) {
-            jobIdsFuture.completeExceptionally(
+            jobIdsFuture.obtrudeException(
                     new ApplicationExecutionException("Could not execute application.", t));
         }
     }
